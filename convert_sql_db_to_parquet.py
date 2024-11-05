902219fab36c9220487259b6ef29fbe971f954be
Andrew Ridden-Harper <andrew.ridden-harper@canterbury.ac.nz>
2024-11-06 07:57:17 +1300

    filtered on CPT and SCPT in sql db

--- convert_sql_db_to_parquet.py
+++ convert_sql_db_to_parquet.py
@@ -36,53 +36,63 @@ cpt_with_size_zero = []
 cpt_ids_with_other_exceptions = []
 other_exceptions = []
 
-cpts_dfs_with_valid_size = []
+cpts_and_scpt_dfs_with_valid_size = []
 
 ###  Confirm which CPTs from the previous SQL dataset have a size greater than 0
-for row_n, cpt_loc in tqdm(enumerate(cpt_locs), total=len(cpt_locs)):
-    cpt_records = load_sql_db.get_cpt_data(session, cpt_loc.name, columnwise=False)
+# for row_n, cpt_loc in tqdm(enumerate(cpt_locs), total=len(cpt_locs)):
+#     cpt_records = load_sql_db.get_cpt_data(session, cpt_loc.name, columnwise=False)
+#
+#     if cpt_records.size > 0:
+#         cpts_and_scpt_dfs_with_valid_size.append(cpt_loc.name)
+#
+#
+# valid_cpts_df = pd.DataFrame(cpts_and_scpt_dfs_with_valid_size, columns=["cpt_name"])
+# valid_cpts_df.to_csv(parquet_conversion_meta_output_dir / "valid_cpts.csv")
 
-    if cpt_records.size > 0:
-        cpts_dfs_with_valid_size.append(cpt_loc.name)
-
-valid_cpts_df = pd.DataFrame(cpts_dfs_with_valid_size, columns=["cpt_name"])
-valid_cpts_df.to_csv(parquet_conversion_meta_output_dir / "valid_cpts.csv")
-
-### Convert the CPTs with a size greater than 0 to parquet
-for row_n, cpt_loc in tqdm(enumerate(cpt_locs), total=len(cpt_locs)):
-
-    try:
-
-        cpt_records = load_sql_db.get_cpt_data(session, cpt_loc.name, columnwise=False)
-
-        if cpt_records.size == 0:
-            cpt_with_size_zero.append(cpt_loc.name)
-            continue
-
-        old_df = pd.DataFrame(cpt_records, columns=["Depth", "qc", "fs", "u"])
-
-        old_df.attrs["nztm_x"] = cpt_loc.nztm_x
-        old_df.attrs["nztm_y"] = cpt_loc.nztm_y
-
-        latlon = coordinates.nztm_to_wgs_depth(np.array([cpt_loc.nztm_y, cpt_loc.nztm_x]))
-
-        old_df.attrs["lat"] = latlon[0]
-        old_df.attrs["lon"] = latlon[1]
-
-        old_df.attrs["cpt_name"] = cpt_loc.name
-
-        old_df.insert(0, "record_name", cpt_loc.name)
-        old_df.insert(1, "latitude", latlon[0])
-        old_df.insert(2, "longitude", latlon[1])
-
-        old_df.to_parquet(parquet_output_dir / f"{cpt_loc.name}.parquet")
-
-    except Exception as e:
-        cpt_ids_with_other_exceptions.append(cpt_loc.name)
-        other_exceptions.append(e)
+valid_cpts_and_scpts_df = pd.read_csv(parquet_conversion_meta_output_dir / "valid_cpts.csv")
+valid_cpts = valid_cpts_and_scpts_df[~valid_cpts_and_scpts_df['cpt_name'].str.contains('SCPT')]
+valid_scpts = valid_cpts_and_scpts_df[valid_cpts_and_scpts_df['cpt_name'].str.contains('SCPT')]
 
 
-cpt_with_size_zero = pd.DataFrame(cpt_with_size_zero, columns=["cpt_name"])
-cpt_with_size_zero.to_csv(parquet_conversion_meta_output_dir / "cpt_with_size_zero.csv")
+print()
 
-cpt_ids_with_other_exceptions = pd.DataFrame({"cpt_name": cpt_ids_with_other_exceptions, "exception": other_exceptions})
+
+#
+# ### Convert the CPTs with a size greater than 0 to parquet
+# for row_n, cpt_loc in tqdm(enumerate(cpt_locs), total=len(cpt_locs)):
+#
+#     try:
+#
+#         cpt_records = load_sql_db.get_cpt_data(session, cpt_loc.name, columnwise=False)
+#
+#         if cpt_records.size == 0:
+#             cpt_with_size_zero.append(cpt_loc.name)
+#             continue
+#
+#         old_df = pd.DataFrame(cpt_records, columns=["Depth", "qc", "fs", "u"])
+#
+#         old_df.attrs["nztm_x"] = cpt_loc.nztm_x
+#         old_df.attrs["nztm_y"] = cpt_loc.nztm_y
+#
+#         latlon = coordinates.nztm_to_wgs_depth(np.array([cpt_loc.nztm_y, cpt_loc.nztm_x]))
+#
+#         old_df.attrs["lat"] = latlon[0]
+#         old_df.attrs["lon"] = latlon[1]
+#
+#         old_df.attrs["cpt_name"] = cpt_loc.name
+#
+#         old_df.insert(0, "record_name", cpt_loc.name)
+#         old_df.insert(1, "latitude", latlon[0])
+#         old_df.insert(2, "longitude", latlon[1])
+#
+#         old_df.to_parquet(parquet_output_dir / f"{cpt_loc.name}.parquet")
+#
+#     except Exception as e:
+#         cpt_ids_with_other_exceptions.append(cpt_loc.name)
+#         other_exceptions.append(e)
+#
+#
+# cpt_with_size_zero = pd.DataFrame(cpt_with_size_zero, columns=["cpt_name"])
+# cpt_with_size_zero.to_csv(parquet_conversion_meta_output_dir / "cpt_with_size_zero.csv")
+#
+# cpt_ids_with_other_exceptions = pd.DataFrame({"cpt_name": cpt_ids_with_other_exceptions, "exception": other_exceptions})
